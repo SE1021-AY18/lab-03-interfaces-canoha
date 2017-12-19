ad1b1133c7fb51f2eaa48c07d41ecfa8baaa3bf9
canoha <34043454+canoha@users.noreply.github.com>
2017-12-19 14:04:40 -0600

    3rd commit

--- src/Assembly.java
+++ src/Assembly.java
@@ -15,38 +15,86 @@ import java.util.ArrayList;
  * @version 1
  */
 public class Assembly implements Part {
-
-    DecimalFormat costFormat = new DecimalFormat();
+    private final DecimalFormat costFormat = new DecimalFormat("0.00");
+    private final DecimalFormat weightFormat = new DecimalFormat("#.###");
+    private ArrayList<Part> subParts = new ArrayList<>();
     private String name;
-    ArrayList<Part> subParts = new ArrayList<>();
+    /**
+     * A constant used to add 25 cents for each sub part used
+     */
     public static final double USD_PER_SUB_PART = 0.25;
-    DecimalFormat weightFormat = new DecimalFormat();
 
+    /**
+     * Adds a part to the assembly's list
+     * @param part added to the ArrayList
+     */
     public void addPart(Part part){
-        //TODO
+        subParts.add(part);
     }
 
+    /**
+     * Default constructor
+     * @param name of the assembly
+     */
     public Assembly(String name){
-        //TODO
+        this.name = name;
     }
 
+    /**
+     * Gets the cost of the assembly in total with all parts included
+     * @return the total cost of the assembly
+     */
     @Override
-    public double getCost() {
-        return 0;
+    public double getCost(){
+        double totalCost = 0.0;
+        for (int i=0; i<subParts.size();i++){
+            totalCost += subParts.get(i).getCost() + USD_PER_SUB_PART;
+        }
+        return totalCost;
     }
 
+    /**
+     * Returns the name of the assembly
+     * @return the name of assembly
+     */
     @Override
-    public String getName() {
-        return name;
+    public String getName(){
+        return this.name;
     }
 
+    /**
+     * Returns the total weight of the assembly
+     * @return the total weight of the assembly
+     */
     @Override
-    public double getWeight() {
-        return 0;
+    public double getWeight(){
+        double totalWeight = 0;
+        for (int i=0; i<subParts.size(); i++){
+            totalWeight += subParts.get(i).getWeight();
+        }
+        return totalWeight;
     }
 
+    /**
+     * Prints out the bill of materials which contains information regarding the assembly object,
+     * including total weight, total cost, and information about the sub parts of the assembly.
+     */
     @Override
     public void printBillOfMaterials() {
+        System.out.println("==========================");
+        System.out.println(getName());
+        System.out.println("==========================");
 
+        for (int i=0; i<subParts.size();i++){
+            System.out.println("Part: "+ subParts.get(i).getName());
+            System.out.println("Cost: "+ costFormat.format(subParts.get(i).getCost()));
+            System.out.println("Weight: "+ weightFormat.format(subParts.get(i).getWeight())+" lbs");
+        }
+        System.out.println("Total cost: $"+ costFormat.format(getCost()));
+        System.out.println("Total weight: "+ weightFormat.format(getWeight())+ " lbs");
+        System.out.println();
+        for (int i=0; i<subParts.size(); i++){
+            subParts.get(i).printBillOfMaterials();
+        }
     }
 }
--- src/Duplicate.java
+++ src/Duplicate.java
@@ -9,20 +9,33 @@
 import java.text.DecimalFormat;
 
 /**
- * This class has to do with duplicates
+ * This class allows for a specific part to be used but with multiple copies
  * @author Harim Cano
  * @version 1
  */
 public class Duplicate implements Part {
-
+    private final DecimalFormat weightFormat = new DecimalFormat("#.###");
+    private final DecimalFormat costFormat = new DecimalFormat("0.00");
     private int numDuplicates;
-    public static final double REDUCTION_FACTOR1 = 0.95;
-    public static final double REDUCTION_FACTOR2 = 0.9;
-    public static final int USD_THRESHOLD1 = 5;
-    public static final int USD_THRESHOLD2 = 10;
-    DecimalFormat weightFormat = new DecimalFormat();
-    DecimalFormat costFormat = new DecimalFormat();
     private Part identicalPart;
+    /**
+     * A constant that is used for calculating the discount for a person that
+     * orders at least 5 identical parts.
+     */
+    public static final double REDUCTION_FACTOR1 = 0.95;
+    /**
+     * A constant that is used to calculating the discount for a person that
+     * orders at least 10 identical parts.
+     */
+    public static final double REDUCTION_FACTOR2 = 0.9;
+    /**
+     * A constant for how many items a person has to order to get discount #1
+     */
+    public static final int USD_THRESHOLD1 = 5;
+    /**
+     *A constant for how many items a person has to order to get discount #2
+     */
+    public static final int USD_THRESHOLD2 = 10;
 
 
     public Duplicate(Part identicalPart, int numDuplicates){
